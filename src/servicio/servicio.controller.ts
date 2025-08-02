c52bc32b577ee837151df6e38e296e36758a5ce5
EliasMoramarco <emoramarco@frro.utn.edu.ar>
2025-08-01 21:25:11 -0300

    Update 9:24 PM 01/08

--- src/servicio/servicio.controller.ts
+++ src/servicio/servicio.controller.ts
@@ -3,8 +3,7 @@ import { ServicioRepository } from './servicio.repository.js'
 import { Servicio } from './servicio.entity.js'
 
 const repository = new ServicioRepository()
-//magui veni explicame esto que no lo entiendo 
-//se encarga de limpiar o validar los datos de entrada relacionados con el servicio Guardarlos en una base de datos, Enviarlos a una API, O usarlos en la lógica de negocio.
+
 function sanitizeServicioInput(req: Request, res: Response, next: NextFunction) {
   req.body.sanitizedInput = {
     name: req.body.name,
@@ -20,43 +19,44 @@ function sanitizeServicioInput(req: Request, res: Response, next: NextFunction) 
 }
 
 //Devuelve todos los servicios que hay en el repositorio. Responde con un JSON que tiene los servicios.
-function findAll(req: Request, res: Response) {
+async function findAll(req: Request, res: Response) {
   res.json({ data: repository.findAll() })
 }
 
-// La función findOne sirve para buscar un único servicio del sistema, usando el código que se pasa por la URL. Si lo encuentra, responde con un JSON que contiene los datos del servicio.
-function findOne(req: Request, res: Response) {
+async function findOne(req: Request, res: Response) {
   const codigo = req.params.codigo
-  const servicio = repository.findOne({ codigo })
+  const servicio = await repository.findOne({ codigo })
   if (!servicio) {
     /*return*/ res.status(404).send({ message: 'Servicio not found' })
   }
   res.json({ data: servicio })
 }
 
-//obtiene los datos sanitizados a traves del middleware sanitizeServicioInput, crea una nueva instancia de Servicio. Usa el metodo repository.add(servicioInput) para guardar el servicio y retorna un JSON con el mensaje de éxito y los datos del servicio creado.
-function add(req: Request, res: Response) {
+async function add(req: Request, res: Response) {
   const input = req.body.sanitizedInput
-  const servicioInput = new Servicio(
+
+  const servicioInput = new Servicio (
     input.name,
-    input.tiempoDemora
+    input.tiempoDemora,
+    input.codigo
     )
-  const servicio = repository.add(servicioInput)
-  /*return*/ res.status(201).send({ message: 'Servicio created', data: servicio })
+
+  const servicio = await  repository.add(servicioInput)
+  res.status(201).send({ message: 'Servicio created', data: servicio })
 }
 
 
 // La función update busca un servicio en el repositorio usando el código que se pasa por la URL. Si lo encuentra, actualiza los datos del servicio, da un mensaje de exito y muestra los cambios. Si no lo encuentra, responde con un mensaje de error.
-function update(req: Request, res: Response) {
+async function update(req: Request, res: Response) {
   req.body.sanitizedInput.codigo = req.params.codigo
-  const servicio = repository.update(req.body.sanitizedInput)
+  const servicio = await repository.update(req.params.codigo, req.body.sanitizedInput)
 
   if (!servicio) {
-    /*return*/ res.status(404).send({ message: 'Servicio not found' })
+    res.status(404).send({ message: 'Servicio not found' })
+  } else {
+    res.status(200).send({ message: 'Servicio updated successfully', data: servicio })
   }
-  /*return*/ res.status(200).send({ message: 'Servicio updated successfully', data: servicio })
 }
-
 
 function remove(req: Request, res: Response) {
   const codigo = req.params.codigo
@@ -70,6 +70,3 @@ function remove(req: Request, res: Response) {
 }
 
 export { sanitizeServicioInput, findAll, findOne, add, update, remove }
-
-// si algo se rompe, es porque le cambie el nombre por controller
-// los return estan comentados porque si no, no funca. tendrian que ir ya que el return es para que no siga ejecutando el resto de la funcion 
