b8054537af813acc671a0a193207ce5157261acc
JeroC2004 <corvijeronimo@gmail.com>
2025-08-04 09:08:37 -0300

    actualizacion crud tono y servicio

--- src/servicio/servicio.entity.ts
+++ src/servicio/servicio.entity.ts
@@ -11,6 +11,7 @@ import {
 } from '@mikro-orm/core'
 import { Precio } from '../precio/precio.entity.js'
 import { Producto } from '../producto/producto.entity.js'
+import { Tono } from '../tono/tono.entity.js'
 
 @Entity()
 export class Servicio {
@@ -28,6 +29,11 @@ export class Servicio {
 
   @ManyToMany(() => Producto, producto => producto.servicios, { cascade: [Cascade.ALL], owner: true })
   productos = new Collection<Producto>(this);
+
+   @ManyToMany(() => Tono, tono => tono.servicios, { cascade: [Cascade.ALL] })
+  tonos = new Collection<Tono>(this);
+
+
 
   //falta relacion con tono y con atencion
 }
--- src/tono/tono.entity.ts
+++ src/tono/tono.entity.ts
@@ -5,13 +5,25 @@ import {
   Cascade,
   ManyToOne,
   Rel,
+  Collection
 } from '@mikro-orm/core'
 import { BaseEntity } from '../shared/baseEntity.entity.js'
+import { Producto } from '../producto/producto.entity.js';
+import { Servicio } from '../servicio/servicio.entity.js';
 
 @Entity()
 export class Tono extends BaseEntity {
   
   @Property({ nullable: false })
   nombre!: String
+
+   @ManyToMany(() => Servicio, servicio => servicio.tonos, {cascade: [Cascade.ALL] }) 
+    servicios = new Collection<Servicio>(this);
+
+    
+
+
+
+
 
 }
