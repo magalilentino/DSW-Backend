6aefb6ea7f1dd3e08793df3f27c266739c3573f1
JeroC2004 <corvijeronimo@gmail.com>
2026-04-15 19:38:01 -0300

    CRUD descuentos

--- src/descuento/descuento.controller.ts
+++ src/descuento/descuento.controller.ts
@@ -4,15 +4,13 @@ import { orm } from '../shared/orm.js'
 
 const em = orm.em
 
-function sanitizeDescuentoInput(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+function sanitizeDescuentoInput(req: Request, res: Response, next: NextFunction) {
   req.body.sanitizedInput = {
-    porcentaje: req.body.nombreporcentaje,
-    cantAtencionNecesaria: req.body.cantAtencionNecesaria
+    porcentaje: req.body.porcentaje,
+    cantAtencionNecesaria: req.body.cantAtencionNecesaria,
+    estado: req.body.estado 
   }
+  
   Object.keys(req.body.sanitizedInput).forEach((key) => {
     if (req.body.sanitizedInput[key] === undefined) {
       delete req.body.sanitizedInput[key]
@@ -23,11 +21,8 @@ function sanitizeDescuentoInput(
 
 async function findAll(req: Request, res: Response) {
   try {
-    const descuentos = await em.find(
-      Descuento,
-      {},
-    )
-    res.status(200).json({ message: 'found all escuento', data: descuentos })
+    const descuentos = await em.find(Descuento, {})
+    res.status(200).json({ message: 'found all descuentos', data: descuentos })
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
@@ -35,13 +30,10 @@ async function findAll(req: Request, res: Response) {
 
 async function findOne(req: Request, res: Response) {
   try {
-    const idDescuento = Number.parseInt(req.params.idDescuento)
-    const descuento = await em.findOneOrFail(
-      Descuento,
-      { idDescuento },
-      //{ populate: ['tonos', 'productos', 'precios', 'clientes'] }
-    )
-    res.status(200).json({ message:'found descuento', data: descuento })
+    // CAMBIO CLAVE: req.params.idDescuento para que coincida con tu Router
+    const idDescuento = Number.parseInt(req.params.idDescuento) 
+    const descuento = await em.findOneOrFail(Descuento, { idDescuento })
+    res.status(200).json({ message: 'found descuento', data: descuento })
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
@@ -51,7 +43,7 @@ async function add(req: Request, res: Response) {
   try {
     const descuento = em.create(Descuento, req.body.sanitizedInput)
     await em.flush()
-    res.status(201).json({ message:'descuento created', data: descuento })
+    res.status(201).json({ message: 'descuento created', data: descuento })
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
@@ -59,13 +51,14 @@ async function add(req: Request, res: Response) {
 
 async function update(req: Request, res: Response) {
   try {
-    const idDescuento = Number.parseInt(req.params.idDescuent)
+    // CAMBIO CLAVE: req.params.idDescuento para que coincida con tu Router
+    const idDescuento = Number.parseInt(req.params.idDescuento)
     const descuentoToUpdate = await em.findOneOrFail(Descuento, { idDescuento })
+    
     em.assign(descuentoToUpdate, req.body.sanitizedInput)
     await em.flush()
-    res
-      .status(200)
-      .json({ message:'descuento updated',data: descuentoToUpdate })
+    
+    res.status(200).json({ message: 'descuento updated', data: descuentoToUpdate })
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
@@ -73,9 +66,11 @@ async function update(req: Request, res: Response) {
 
 async function remove(req: Request, res: Response) {
   try {
+    // CAMBIO CLAVE: req.params.idDescuento para que coincida con tu Router
     const idDescuento = Number.parseInt(req.params.idDescuento)
-    const descuento = em.findOneOrFail(Descuento, { idDescuento })
+    const descuento = await em.findOneOrFail(Descuento, { idDescuento })
     await em.removeAndFlush(descuento)
+    res.status(200).json({ message: 'descuento deleted' })
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
